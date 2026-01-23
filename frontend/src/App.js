3d8b53d835296874833a2ceae3ae243295d66fef
Empsofttech <empsofttech99@gmail.com>
2026-01-23 22:15:24 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import JavaService from "./pages/services/JavaService";
+import PythonService from "./pages/services/PythonService";
+import DevOpsService from "./pages/services/DevOpsService";
+import TestingService from "./pages/services/TestingService";
+import Ai from "./pages/services/AiService";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/java" element={<JavaService />} />
+        <Route path="/services/python" element={<PythonService />} />
+        <Route path="/services/devops" element={<DevOpsService />} />
+        <Route path="/services/testing" element={<TestingService />} />
+        <Route path="/services/artifical" element={<Ai />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
